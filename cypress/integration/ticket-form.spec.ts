8f05dc812f08bc81dc7aa41939252247404c1e43
kamilscislo <kamil.m.scislo@gmail.com>
2022-08-31 17:57:53 +0200

    added integration tests

--- cypress/integration/ticket-form.spec.ts
+++ cypress/integration/ticket-form.spec.ts
@@ -1,1 +1,34 @@
 // Your tests go in here. Happy coding! 🤓
+describe('Integration tests for ticket form', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('Form is successfully sent', () => {
+        cy.get('#name').type('Test name')
+        cy.get('#email').type('test.email@email.com')
+        cy.get('#subject').type('Test subject')
+        cy.get('#message').type('Test message')
+        cy.contains('button', 'Submit').click()
+
+        cy.intercept('POST', '/v2/tickets/new', {
+            statusCode: 200,
+            body: { id: 'ABCD' },
+        })
+        cy.contains('Thank you!').should('be.visible')
+    })
+
+    it('Form fails to be sent', () => {
+        cy.get('#name').type('Test name')
+        cy.get('#email').type('test.email@email.com')
+        cy.get('#subject').type('Test subject')
+        cy.get('#message').type('Test message')
+        cy.contains('button', 'Submit').click()
+
+        cy.intercept('POST', '/v2/tickets/new', {
+            statusCode: 500,
+            body: { error: 'Internal server error' },
+        })
+        cy.contains('Error!').should('be.visible')
+    })
+})
