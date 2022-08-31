68c71384ef5b5ba51dadd50e48472adfd27ea053
kamilscislo <kamil.m.scislo@gmail.com>
2022-08-31 22:30:18 +0200

    moved selectors to beforeEach

--- cypress/integration/ticket-form.spec.ts
+++ cypress/integration/ticket-form.spec.ts
@@ -1,34 +1,29 @@
 // Your tests go in here. Happy coding! 🤓
-describe('Integration tests for ticket form', () => {
+describe('Submitting ticket form', () => {
     beforeEach(() => {
         cy.visit('/')
-    })
-
-    it('Form is successfully sent', () => {
         cy.get('#name').type('Test name')
         cy.get('#email').type('test.email@email.com')
         cy.get('#subject').type('Test subject')
         cy.get('#message').type('Test message')
         cy.contains('button', 'Submit').click()
+    })
 
+    it('Form is successfully sent', () => {
         cy.intercept('POST', '*/tickets/new', {
             statusCode: 200,
             body: { id: 'ABCD' },
-        })
+        }).as('FormSuccess')
+        cy.wait('@FormSuccess')
         cy.contains('Thank you!').should('be.visible')
     })
 
     it('Form fails to be sent', () => {
-        cy.get('#name').type('Test name')
-        cy.get('#email').type('test.email@email.com')
-        cy.get('#subject').type('Test subject')
-        cy.get('#message').type('Test message')
-        cy.contains('button', 'Submit').click()
-
         cy.intercept('POST', '*/tickets/new', {
             statusCode: 500,
             body: { error: 'Internal server error' },
-        })
+        }).as('FormFail')
+        cy.wait('@FormFail')
         cy.contains('Error!').should('be.visible')
     })
 })
